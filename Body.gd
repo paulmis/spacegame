04e8d177198234cd20a4d2056b3aab35b7872055
paulmis <chugdevs@gmail.com>
2023-01-02 03:59:31 +0100

    add collisions, UI icons

--- Body.gd
+++ Body.gd
@@ -7,10 +7,16 @@ var period: float
 var radius: float
 var distance: float
 var orbit_center: Vector3
+var primary: Body_
+var satelites: Array = []
 onready var sphere = $Sphere
+onready var collision = $CollisionShape
+onready var dialog = get_node("/root/World/BodyDialog")
 onready var orbit = $Orbit
+onready var detail = get_node("/root/World/DetailCheckBox")
 
-func init(name: String, mass: int, radius: float, period: float, distance: float, orbit_center: Vector3):
+func init(name: String, mass: int, radius: float, period: float, 
+		  distance: float, orbit_center: Vector3):
 	self.name = name
 	self.mass = mass
 	self.radius = radius
@@ -19,11 +25,22 @@ func init(name: String, mass: int, radius: float, period: float, distance: float
 	self.orbit_center = orbit_center
 	
 func _ready():
-	sphere.mesh.radius = radius
-	sphere.mesh.height = radius * 2
+	var mesh = SphereMesh.new()
+	mesh.radius = radius
+	mesh.height = radius * 2
+	sphere.mesh = mesh
+	
+	var collisionShape = SphereShape.new()
+	collisionShape.radius = radius
+	collision.shape = collisionShape
+	
 	orbit.inner_radius = distance
-	orbit.outer_radius = distance + 0.04
+	orbit.outer_radius = distance + 0.1
 	calculate_position()
+	
+func add_satelite(satelite: Body_):
+	satelite.primary = self
+	self.satelites.append(satelite)
 
 func calculate_position():
 	self.transform.origin = self.orbit_center +\
@@ -34,3 +51,19 @@ func calculate_position():
 
 func _process(_delta):
 	self.calculate_position()
+	orbit.visible = detail.is_pressed()
+
+
+func _on_RigidBody_mouse_entered():
+	dialog.get_node("Name").text = "Name: " + name
+	if primary:	
+		dialog.get_node("PrimaryName").text = "Primary: " + primary.name
+	else:
+		dialog.get_node("PrimaryName").text = ""
+	dialog.get_node("Period").text = str(period)
+		
+	dialog.show()
+
+
+func _on_RigidBody_mouse_exited():
+	dialog.hide()
--- World.gd
+++ World.gd
@@ -1,17 +1,28 @@
 extends Spatial
 
-var bodies = [
-	["Vaie", 2300, .3, 10, 32.4, Vector3(0, 0, 0)],
-	["Tentoe", 4500, .6, 12, 46.4, Vector3(0, 0, 0)]
+var data = [
+	["Do-Tah", 99000, 3.4, .001, .001, Vector3(0, 0, 0)],
+	["Vaie", 2300, .4, 10, 31.4, Vector3(0, 0, 0)],
+	["Tentoe", 4500, .8, 12, 42.4, Vector3(0, 0, 0)],
+	["Aarion", 345, .3, 35, 68.4, Vector3(0, 0, 0)]
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var Body_ = preload("res://Body.tscn")
-	for params in bodies:
+	var bodies = []
+	for params in data:
 		var body = Body_.instance()
 		body.init(params[0], params[1], params[2], params[3], params[4], params[5])
-		add_child(body)
+		bodies.append(body)
+	
+	bodies[0].add_satelite(bodies[1])
+	bodies[0].add_satelite(bodies[2])
+	bodies[0].add_satelite(bodies[3])
+	add_child(bodies[0])
+	add_child(bodies[1])
+	add_child(bodies[2])
+	add_child(bodies[3])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
